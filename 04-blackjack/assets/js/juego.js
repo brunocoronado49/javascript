e344f06866b97a4972ef89bf8d66c139da256dba
brunocoronado49 <francorangelcoronado@gmail.com>
2023-07-07 23:17:34 -0600

    manipulando el dom

--- 04-blackjack/assets/js/juego.js
+++ 04-blackjack/assets/js/juego.js
@@ -1,6 +1,11 @@
 let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
+let puntosJugador = 0;
+let puntosComputadora = 0;
+
+const btnPedir = document.getElementById('btnPedir');
+const marcador = document.querySelectorAll('small');
 
 const crearDeck = () => {
     for (let i = 2; i <= 10; i++) {
@@ -15,11 +20,35 @@ const crearDeck = () => {
         }
     }
 
-    console.log(deck);
     deck = _.shuffle(deck);
-    console.log(deck);
 
     return deck;
 }
 
 crearDeck();
+
+const pedirCarta = () => {
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+
+    const carta = deck.pop();
+    return carta;
+}
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ? 
+        ((valor === 'A') ? 11 : 10) 
+        : valor * 1; 
+
+}
+
+const valor = valorCarta(pedirCarta());
+
+// eventos
+btnPedir.addEventListener('click', function() {
+   const carta = pedirCarta();
+   puntosJugador = puntosJugador + valorCarta(carta);
+   marcador[0].innerText = puntosJugador;
+});
