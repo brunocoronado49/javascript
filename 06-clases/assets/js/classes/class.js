783645d8d251c29298892032731b4364e38a3be3
brunocoronado49 <francorangelcoronado@gmail.com>
2023-07-11 12:31:18 -0600

    clases con propiedades y metodos estaticos

--- 06-clases/assets/js/classes/class.js
+++ 06-clases/assets/js/classes/class.js
@@ -1,4 +1,14 @@
 class Persona {
+    static _conteo = 0;
+    static get conteo() {
+        return Persona._conteo + " Instancias";
+    }
+
+    static mensaje() {
+        console.log(this.nombre); // undefind
+        console.log("Hola soy estatico");
+    }
+
     nombre = "";
     codigo = "";
     frase = "";
@@ -9,6 +19,8 @@ class Persona {
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+
+        Persona._conteo++;
     }
 
     set setComidaFavorita(comida) {
@@ -31,14 +43,25 @@ class Persona {
 
 const spiderman = new Persona("Peter Parker", "Spiderman", "Tu vecino we");
 const ironman = new Persona("Tony Stark", "Ironman", "Arriba las milfs");
-console.log(ironman);
+
+// console.log(ironman);
 
 spiderman.quienSoy();
 spiderman.miFrase();
-ironman.quienSoy();
-ironman.miFrase();
+// ironman.quienSoy();
+// ironman.miFrase();
 
 spiderman.setComidaFavorita = "El pay de cereza";
-spiderman.nemesis = "venom";
-console.log(spiderman);
-console.log(spiderman.getComidaFavorita);
+// spiderman.nemesis = "venom";
+// console.log(spiderman.getComidaFavorita);
+// console.log(spiderman);
+
+// Persona._conteo = 2;
+console.log("Conteo estatico:", Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
+
+// estatica fuera de clase
+Persona.propiedadExterna = "Hola Mundo";
+console.log(Persona);
+console.log(Persona.propiedadExterna);
