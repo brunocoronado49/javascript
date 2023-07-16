554985f978fac40d24ffe2cb7f8dc3b3a62e375c
brunocoronado49 <francorangelcoronado@gmail.com>
2023-07-16 14:47:32 -0600

    creando la carpeta de prod

--- 07-modulos/blackjack-vite/main.js
+++ 07-modulos/blackjack-vite/main.js
@@ -70,7 +70,7 @@ const turnoComputadora = (puntosMinimos) => {
 
         // <img class="carta" src="assets/cartas/2C.png">
         const imgCarta = document.createElement("img");
-        imgCarta.src = `./assets/cartas/${carta}.png`; //3H, JD
+        imgCarta.src = `./public/assets/cartas/${carta}.png`; //3H, JD
         imgCarta.classList.add("carta");
         divCartasComputadora.append(imgCarta);
 
