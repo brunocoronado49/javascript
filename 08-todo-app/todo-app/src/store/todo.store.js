ec7fe75b72f42669b59786841b598cee5aa95400
brunocoronado49 <francorangelcoronado@gmail.com>
2023-07-25 23:21:49 -0600

    terminando el todo store

--- 08-todo-app/todo-app/src/store/todo.store.js
+++ 08-todo-app/todo-app/src/store/todo.store.js
@@ -10,7 +10,9 @@ const state = {
     todos: [
         new Todo("Piedra del alma"),
         new Todo("Piedra del infinito"),
-        new Todo("Piedra del tiempo")
+        new Todo("Piedra del tiempo"),
+        new Todo("Piedra del poder"),
+        new Todo("Piedra del realidad")
     ],
     filter: Filters.All
 };
@@ -29,35 +31,60 @@ const loadStore = () => {
  * @param {String} description
  */
 const addTodo = (description) => {
-    throw new Error("No implemented");
+    if (!description) throw new Error("Description is required");
+    state.todos.push(new Todo(description));
+};
+
+const getTodos = (filter = Filters.All) => {
+    switch (filter) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter((todo) => todo.done);
+        case Filters.Pending:
+            return state.todos.filter((todo) => !todo.done);
+        default:
+            throw new Error("Option is not valid.");
+    }
 };
 
 const toggleTodo = (todoId) => {
-    throw new Error("No implemented");
+    state.todos = state.todos.map((todo) => {
+        if (todo.id === todoId) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
 };
 
 const deleteTodo = (todoId) => {
-    throw new Error("No implemented");
+    state.todos = state.todos.filter((todo) => todo.id != todoId);
 };
 
 const deleteCompleted = () => {
-    throw new Error("No implemented");
+    state.todos = state.todos.filter((todo) => todo.done);
 };
 
+/**
+ *
+ * @param {Filters} newFilter
+ */
 const setFilter = (newFilter = Filters.All) => {
-    throw new Error("No implemented");
+    state.filter = newFilter;
 };
 
 const getCurrentFlter = () => {
-    throw new Error("No implemented");
+    return state.filter;
 };
 
 export default {
-    initStore,
     addTodo,
-    toggleTodo,
-    deleteTodo,
     deleteCompleted,
+    deleteTodo,
+    getCurrentFlter,
+    getTodos,
+    initStore,
+    loadStore,
     setFilter,
-    getCurrentFlter
+    toggleTodo
 };
