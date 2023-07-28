f3ba58a8f030c6f000a8e4927c89281e8cd03944
brunocoronado49 <francorangelcoronado@gmail.com>
2023-07-28 09:47:54 -0600

    renderizando la lista de todos, pendiente

--- 08-todo-app/todo-app/src/todos/app.js
+++ 08-todo-app/todo-app/src/todos/app.js
@@ -1,14 +1,20 @@
 import html from "./app.html?raw";
+import todoStore from "../store/todo.store";
 
 /**
  *
  * @param {String} elementId
  */
 export const App = (elementId) => {
+    const displayTodos = () => {
+        const todos = todoStore.getTodos();
+    };
+
     // cuando la funcion App se llama
     (() => {
         const app = document.createElement("div");
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodos();
     })();
 };
